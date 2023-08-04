c36a5eb462d20a1914959749bd1c3a1465e4c16b
Mekintosz <michal.lazicki@gmail.com>
2023-08-04 11:12:03 +0200

    + decimal button, rounding function, app fully function but throws an error

--- script.js
+++ script.js
@@ -5,35 +5,35 @@ display.innerText = currentDisplay
 let numFirst = "";
 let operators = "";
 
-const num1 = document.querySelector(".b1")
-const num2 = document.querySelector(".b2")
-const num3 = document.querySelector(".b3")
-const num4 = document.querySelector(".b4")
-const num5 = document.querySelector(".b5")
-const num6 = document.querySelector(".b6")
-const num7 = document.querySelector(".b7")
-const num8 = document.querySelector(".b8")
-const num9 = document.querySelector(".b9")
-const num0 = document.querySelector(".b0")
+const num1 = document.querySelector(".b1");
+const num2 = document.querySelector(".b2");
+const num3 = document.querySelector(".b3");
+const num4 = document.querySelector(".b4");
+const num5 = document.querySelector(".b5");
+const num6 = document.querySelector(".b6");
+const num7 = document.querySelector(".b7");
+const num8 = document.querySelector(".b8");
+const num9 = document.querySelector(".b9");
+const num0 = document.querySelector(".b0");
+const point = document.querySelector(".point");
 
-num1.addEventListener("click", () => updateDisplay(1))
-num2.addEventListener("click", () => updateDisplay(2))
-num3.addEventListener("click", () => updateDisplay(3))
-num4.addEventListener("click", () => updateDisplay(4))
-num5.addEventListener("click", () => updateDisplay(5))
-num6.addEventListener("click", () => updateDisplay(6))
-num7.addEventListener("click", () => updateDisplay(7))
-num8.addEventListener("click", () => updateDisplay(8))
-num9.addEventListener("click", () => updateDisplay(9))
+num1.addEventListener("click", () => updateDisplay(1));
+num2.addEventListener("click", () => updateDisplay(2));
+num3.addEventListener("click", () => updateDisplay(3));
+num4.addEventListener("click", () => updateDisplay(4));
+num5.addEventListener("click", () => updateDisplay(5));
+num6.addEventListener("click", () => updateDisplay(6));
+num7.addEventListener("click", () => updateDisplay(7));
+num8.addEventListener("click", () => updateDisplay(8));
+num9.addEventListener("click", () => updateDisplay(9));
 num0.addEventListener("click", () => updateDisplay(0))
+point.addEventListener("click", () => updateDisplay("."));
 
 const add = document.querySelector(".add")
 const subtract = document.querySelector(".subtract")
 const multiply = document.querySelector(".multiply")
 const divide = document.querySelector(".divide")
 const equal = document.querySelector(".equal")
-
-
 
 add.addEventListener("click", () => gatherData("+"))
 subtract.addEventListener("click", () => gatherData("-"))
@@ -47,6 +47,13 @@ const reset = document.querySelector(".reset")
 clear.addEventListener("click", () => clearLast())
 reset.addEventListener("click", () => resetAll())
 
+const calc = document.querySelector(".calc");
+calc.addEventListener("click", () => checkDot())
+
+const formatter = new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: 0,      
+    maximumFractionDigits: 4,
+ });
 
 function operate(numA, numB, operator) {
     let result = 0;
@@ -65,7 +72,7 @@ function operate(numA, numB, operator) {
            break;
     };
 
-    result = result.toString()
+    result = formatter.format(result)
     display.innerText = `${result}`;
     currentDisplay = result
 }
@@ -76,7 +83,9 @@ function updateDisplay(call) {
     } else {
         display.innerText = `${currentDisplay}${call}`
     }
+    
     currentDisplay = display.innerText
+    
 }
 
 function gatherData(oper) {
@@ -99,4 +108,12 @@ function resetAll() {
     operators = "";
     currentDisplay = 0;
     display.innerText = 0;
+}   
+
+function checkDot() {
+    if (currentDisplay.includes(".")) {
+        point.disabled = true
+    } else {
+        point.disabled = false
+    }
 }
